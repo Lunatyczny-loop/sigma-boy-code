6fe4b73ea75f089bd5393fd9291dd54830df2cb2
Lunatyczny-loop <szymonrabich11@gmail.com>
2025-10-18 18:35:38 +0200

    new file:   SigmaBoy/Program.cs 	new file:   SigmaBoy/SigmaBoy.csproj

---
+++ SigmaBoy/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Int32 number1 = Convert.ToInt32(args[0]);
+        Int32 number2 = Convert.ToInt32(args[1]);
+        int
+        Console.WriteLine($"Sumą jest: {number1 + number2}");
+    }
+}
